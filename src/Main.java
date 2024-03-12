286b02aa1b6f57ca12402f151b7a2d592c180408
Christian <christian.torresgaviria@elev.ga.ntig.se>
2024-03-12 18:32:00 +0100

    menu test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    static Scanner input = new Scanner(System.in);
+    static int userInput;
 
-
+    public static void main(String[] args){
+        while(userInput != 5){
+            System.out.println("1");
+            System.out.println("2");
+            System.out.println("3");
+            System.out.println("4");
+            System.out.println("5");
+            userInput = input.nextInt();
+        }
     }
 }
